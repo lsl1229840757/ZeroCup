d71718c886c06ffb3772b238097af856dcc36dc6
simple <2309780282@qq.com>
2018-11-20 11:21:31 +0800

    导航大致完成

--- WebContent/myjs/index.js
+++ WebContent/myjs/index.js
@@ -1,32 +1,31 @@
-$(function() {
-  var items = $('#navMenu-items').width();
-  var itemSelected = document.getElementsByClassName('navMenu-item');
-  var backgroundColours = ["rgb(133, 105, 156)", "rgb(175, 140, 115)","rgb(151, 167, 109)","rgb(171, 100, 122)","rgb(105, 134, 156)","rgb(152, 189, 169)"]
-  pointerScroll($(itemSelected));
-  $("#navMenu-items").scrollLeft(200).delay(200).animate({
-    scrollLeft: "-=200"
-  }, 2000, "easeOutQuad"); 
-  $('.navMenu-item').click(function () {
-		$('#navMenu').find('.active').removeClass('active');
-		$(this).addClass("active");
-		pointerScroll($(this));
-		newBackgroundColour(backgroundColours)
-	});
+var items = $('#navMenu-items').width();
+var itemSelected = document.getElementsByClassName('navMenu-item');
+// 使其平移到第一个按钮
+pointerScroll(itemSelected);
+$('.navMenu-item').click(function() {
+	pointerScroll(this);
+	$('.navMenu-item').find('.active').removeClass('active');
+	$(this).find('span').addClass('active');
 });
-var newBackgroundColour = function(backgroundColours){
-  var newBackground = backgroundColours[Math.floor(Math.random()*backgroundColours.length)];
-  if(newBackground != $("#navMenu").css("background-color")){
-    $("#navMenu").css("background",newBackground);
-  }else{
-    newBackgroundColour(backgroundColours);
-  }
-}
 function pointerScroll(ele) {
 	var parentScroll = $("#navMenu-items").scrollLeft();
 	var offset = ($(ele).offset().left - $('#navMenu-items').offset().left);
-	var totalelement = offset + $(ele).outerWidth()/2;
-	var rt = (($(ele).offset().left) - ($('#navMenu-wrapper').offset().left) + ($(ele).outerWidth())/2);
+	var totalelement = offset + $(ele).outerWidth() / 2;
+	var rt = (($(ele).offset().left) - ($('#navMenu-wrapper').offset().left) + ($(ele)
+			.outerWidth()) / 2);
 	$('#menuSelector').animate({
-		left: totalelement + parentScroll
+		left : totalelement + parentScroll
 	})
 }
+
+// 对轮播图进行操作
+$('.slide .circle').click(function() {
+	idx = $(this).index('.slide .circle');
+	$('.slide .circle').not($(this)).removeClass('hit');
+	$(this).addClass('hit');
+	// 对图片进行操作
+	$(".slide img").fadeOut(500);
+	$(".slide img").css('src', 'images/img1.png');
+	$(".slide img").fadeIn(2000);
+})
+
--- WebContent/navigate.jsp
+++ WebContent/navigate.jsp
@@ -7,19 +7,23 @@
 <title>导航界面</title>
 <script type="text/javascript" src="outerjs/jquery-3.3.1.js"></script>
 <script src='outerjs/jquery-ui.min.js'></script>
-<script src='myjs/index.js'></script>
 <link rel="stylesheet" type="text/css" href="mycss/style.css">
 <link rel="stylesheet" type="text/css"
 	href="bootstrap/css/bootstrap.min.css">
 </head>
-<body>    
-<div class="container-fluid">                                                                      
+<body>  
+<canvas id="canvas"></canvas>  
+<div class="container-fluid">  
+	<div class="header"></div>                                                                   
 	<div id="navMenu">
 	    <div id="navMenu-wrapper">
+	    	<div id="logo">
+	    		<img alt="" src="images/logo.png">
+	    	</div>
 	    	<ul id="navMenu-items">
 	        	<div id="menuSelector"></div>
-		        <li class="navMenu-item active">
-		          <span class="item">四季珞珈</span>
+		        <li class="navMenu-item">
+		          <span class="item active">四季珞珈</span>
 		        </li>
 		        <li class="navMenu-item">
 		          <span class="item">学术珞珈</span>
@@ -39,7 +43,14 @@
 	        </ul>
 		</div>
 	</div>
-	<div class="slide"></div>
+	<div class="slide">
+		<div class="circle hit" style="margin-left:20px;"></div>
+		<div class="circle" style="margin-left:50px;"></div>
+		<div class="circle" style="margin-left:80px;"></div>
+		<img alt="" src="images/img1.jpg">
+	</div>
 </div>
+<script src='myjs/index.js'></script>
+<script type="text/javascript" src="outerjs/star.js"></script>
 </body>
 </html>
